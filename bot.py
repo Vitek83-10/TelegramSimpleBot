bd1eb1d1c8dbf8bf2c5915b64d17886f1b755ba5
Vitek83-10 <vnedovesov83@gmail.com>
2025-06-27 12:23:44 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,17 +2,13 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("📸 Бот успешно запущен!")
+    await update.message.reply_text("✅ Бот работает!")
 
-async def status(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("✅ Статус: бот работает!")
-
-if __name__ == '__main__':
-    app = ApplicationBuilder().token(BOT_TOKEN).build()
+if __name__ == "__main__":
+    app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
-    app.add_handler(CommandHandler("status", status))
     print("🚀 Бот запущен!")
     app.run_polling()
